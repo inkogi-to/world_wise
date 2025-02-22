f336d5b24ee396aea327eec3bd27ca9d24583cd2
Nazar Kobernyk <nazarkobernyk@MacBook-Pro-Nazar.local>
2025-02-22 15:34:15 +0000

    fixed bugs end added new factures

--- src/components/CityItem.jsx
+++ src/components/CityItem.jsx
@@ -16,7 +16,7 @@ function CityItem({ city }) {
         className={`${styles.cityItem} ${
           id === currentCity.id ? styles["cityItem--active"] : ""
         }`}
-        to={`${id}?lat${position.lat}&lat=${position.lng}`}
+        to={`${id}?lat=${position.lat}&lng=${position.lng}`}
       >
         <span className={styles.emoji}>{emoji}</span>
         <h3 className={styles.name}>{cityName}</h3>
